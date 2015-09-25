3accd7ba6725b3dbe2eb1660ec68c181c1998ab4
Brian Woodward <brian.woodward@gmail.com>
2015-09-25 00:25:17 -0400

    revert to grunt-assemble code

--- index.js
+++ index.js
@@ -1,70 +1,74 @@
 /**
- * assemble-middleware-wordcount
- * Assemble middleware for rendering the wordcount for
+ * assemble-contrib-wordcount
+ * Assemble plugin for rendering the wordcount for
  * the specified section of content.
  *
- * Copyright (c) 2014 Jon Schlinkert
+ * Copyright (c) 2013 Jon Schlinkert
  * Licensed under the MIT License
  */
-'use strict';
 
 var cheerio = require('cheerio');
-var log = require('verbalize');
 var _ = require('lodash');
 
-module.exports = function (assemble) {
-  var middleware = function (params, next) {
-    var opts  = assemble.config.wordcount || {};
+module.exports = function(params, callback) {
 
-    // See http://onforb.es/1crk3KF
-    opts.speed         = opts.speed         || 300;
-    opts.seconds       = opts.seconds       || false;
-    opts.placement     = opts.placement     || 'prepend';
-    opts.selector      = opts.selector      || '.wordcount';
-    opts.countSelector = opts.countSelector || '.label-wordcount';
-    opts.timeSelector  = opts.timeSelector  || '.label-reading-time';
+  'use strict';
 
-    // load current page content
-    var $ = cheerio.load(params.page.content);
+  var grunt = params.grunt;
+  var opts  = params.assemble.options.wordcount || {};
 
-    if($(opts.selector) && $(opts.selector).length > 0) {
-      var countable = $(opts.selector);
+  // See http://onforb.es/1crk3KF
+  opts.speed         = opts.speed         || 300;
+  opts.seconds       = opts.seconds       || false;
+  opts.placement     = opts.placement     || 'prepend';
+  opts.selector      = opts.selector      || '.wordcount';
+  opts.countSelector = opts.countSelector || '.label-wordcount';
+  opts.timeSelector  = opts.timeSelector  || '.label-reading-time';
 
-      // Strip HTML tags from content
-      var content = countable.html().replace(/(<([^>]+)>)/ig, '');
+  // Skip over the plugin if it isn't defined in the options.
+  grunt.verbose.subhead('Running:'.bold, '"assemble-config-wordcount"');
+  grunt.verbose.writeln('Stage:  '.bold, '"render:post:page"\n');
 
-      // Solution from http://drewschrauf.com/blog/2012/06/13/javascript-wordcount-that-works/
-      var matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g);
-      var count = matches !== null ? matches.length : 0;
+  // load current page content
+  var $ = cheerio.load(params.content);
 
-      // Calculate reading time
-      var min, mins, sec, secs, est;
-      if(opts.seconds === true) {
-        min = Math.floor(count / opts.speed);
-        sec = Math.floor(count % opts.speed / (opts.speed / 60));
-        mins = min + ' minute' + (min === 1 ? '' : 's') + ', ';
-        secs = sec + ' second' + (sec === 1 ? '' : 's');
-        est = (min > 0) ? mins + secs : secs;
-      } else {
-        min = Math.ceil(count / opts.speed);
-        est = min + ' min';
-      }
+  if($(opts.selector) && $(opts.selector).length > 0) {
+    var countable = $(opts.selector);
 
-      // Render wordcount
-      $(opts.countSelector).attr('data-wordcount', count);
-      $(opts.countSelector)[opts.placement](String(count));
+    // Strip HTML tags from content
+    var content = countable.html().replace(/(<([^>]+)>)/ig, '');
 
-      // Render reading time
-      $(opts.timeSelector).attr('data-reading-time', est);
-      $(opts.timeSelector)[opts.placement](est);
+    // Solution from http://drewschrauf.com/blog/2012/06/13/javascript-wordcount-that-works/
+    var matches = content.match(/[\u0400-\u04FF]+|\S+\s*/g);
+    var count = matches !== null ? matches.length : 0;
 
-      params.page.content = $.html();
+    // Calculate reading time
+    var min, mins, sec, secs, est;
+    if(opts.seconds === true) {
+      min = Math.floor(count / opts.speed);
+      sec = Math.floor(count % opts.speed / (opts.speed / 60));
+      mins = min + ' minute' + (min === 1 ? '' : 's') + ', ';
+      secs = sec + ' second' + (sec === 1 ? '' : 's');
+      est = (min > 0) ? mins + secs : secs;
+    } else {
+      min = Math.ceil(count / opts.speed);
+      est = min + ' min';
     }
-    next();
-  };
 
-  middleware.event = 'page:after:render';
-  return {
-    'assemble-middleware-wordcount': middleware
-  };
+    // Render wordcount
+    $(opts.countSelector).attr('data-wordcount', count);
+    $(opts.countSelector)[opts.placement](String(count));
+
+    // Render reading time
+    $(opts.timeSelector).attr('data-reading-time', est);
+    $(opts.timeSelector)[opts.placement](est);
+
+    params.content = $.html();
+  }
+  callback();
+};
+
+
+module.exports.options = {
+  stage: 'render:post:page'
 };
